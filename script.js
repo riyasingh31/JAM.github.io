736fbe1527ce2fbb5f72fa55d95d25ee76a46707
Riya Singh <riyasingh.pdf@gmail.com>
2024-09-24 09:05:33 +0530

    first commit

--- script.js
+++ script.js
@@ -25,6 +25,22 @@ function page4Animation() {
 
 function swiperAnimation() {
     var swiper = new Swiper(".mySwiper", {
+        
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
         slidesPerView: "auto",
         centeredSlides: true,
         spaceBetween: 100,
